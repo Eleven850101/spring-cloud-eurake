32a931d42ae2d1fb89115f74be7983a5e5f00ed8
xiashixin <xiasxjy@163.com>
2019-05-24 17:18:53 +0800

    把配置信息迁移到bootstrap文件中

--- service-order/src/main/java/com/github/summerwind/controller/OrderController.java
+++ service-order/src/main/java/com/github/summerwind/controller/OrderController.java
@@ -1,9 +1,7 @@
 package com.github.summerwind.controller;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
